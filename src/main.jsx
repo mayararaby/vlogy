f4780f8cc982e9d9138477670e30abf2618de2da
Mayar Araby <m.kamal@penta-b.com>
2024-10-23 02:45:34 +0300

    style: root
    add root theme colors

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import store from './redux/state/index'
 import { Provider } from 'react-redux'
+import "./index.css"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
