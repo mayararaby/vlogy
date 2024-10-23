fa78cf1979669e42234df3d78442e48379492f57
Mayar Araby <m.kamal@penta-b.com>
2024-10-23 04:35:36 +0300

    fix: afs
    change ads body

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const ads = [
-    { title: "English course", body: "Improve your English with new course" },
-    { title: "Frontend development course", body: "Improve your development skills new course" },
-    { title: "Cooking course", body: "Improve your cooking skills new course" },
-    { title: "Drawing course", body: "Improve your drawing skills new course" }
+    { title: "English course", body: "Improve your English with a new course" },
+    { title: "Frontend development course", body: "Improve your development skills with a new course" },
+    { title: "Cooking course", body: "Improve your cooking skills with a new course" },
+    { title: "Drawing course", body: "Improve your drawing skills with a new course" }
 ]
